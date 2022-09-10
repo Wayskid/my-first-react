804ff4d3d2066ed0cf80527bdae156c948d3f9e8
Wayskid02 <wayskid02@gmail.com>
2022-09-10 14:46:47 -0500

    first commit

---
+++ src/WaysFirstReact.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function WaysFirstReact() {
+  return (
+    <div>
+        <h1>This is Ways First React Project</h1>
+        <p>I must confess, this all looks weird and I'm asking myself what the creator of React could be thinking when making this. 
+            I like simple html and vanilla js just the way it is. 
+            But then, they say react takes you to the next level. 
+            It makes you become more skilled and very professional. 
+            I think I agree to that so far: It looks very quiet complex 
+            (but in an easy way considering I've learnt the basics of web dev well). 
+            It looks more thorough and professional so far.
+            I hope with time I will see the reason behind using React 
+            and start enjoying it.
+       </p>
+
+       <p>PS. This all looks so ugly considering theres no css yet, I can't wait to learn that.</p>
+    </div>
+  )
+}
+
